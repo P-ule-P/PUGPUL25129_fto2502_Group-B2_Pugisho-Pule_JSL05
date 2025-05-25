6300d03c136bf518fcdd64f18d6eb5d6600174cb
P-ule-P <ppule1@gmail.com>
2025-05-25 11:43:45 +0200

    Updated js to use module and separated initial data from main script

--- scripts.js
+++ scripts.js
@@ -1,44 +1,4 @@
-const initialTasks = [
-  {
-    id: 1,
-    title: "Launch Epic Career 🚀",
-    description: "Create a killer Resume",
-    status: "todo",
-  },
-  {
-    id: 2,
-    title: "Master JavaScript 💛",
-    description: "Get comfortable with the fundamentals",
-    status: "todo",
-  },
-  {
-    id: 3,
-    title: "Keep on Going 🏆",
-    description: "You're almost there",
-    status: "doing",
-  },
-  {
-    id: 11,
-    title: "Learn Data Structures and Algorithms 📚",
-    description:
-      "Study fundamental data structures and algorithms to solve coding problems efficiently",
-    status: "doing",
-  },
-  {
-    id: 12,
-    title: "Contribute to Open Source Projects 🌐",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
-    status: "done",
-  },
-  {
-    id: 13,
-    title: "Build Portfolio Projects 🛠️",
-    description:
-      "Create a portfolio showcasing your skills and projects to potential employers",
-    status: "done",
-  },
-];
+import { initialTasks } from "./initialData.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initializeTasks();
